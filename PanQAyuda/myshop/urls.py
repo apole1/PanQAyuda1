ed9205471b8d6767ee274be054b5bbeef6595616
Oswaldo1002009 <A01206725@itesm.mx>
2019-03-25 23:01:27 -0600

    Error to delete

--- PanQAyuda/myshop/urls.py
+++ PanQAyuda/myshop/urls.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
+from core import views
 
 urlpatterns = [
+    path("home/", views.home, name="home"),
     path('admin/', admin.site.urls),
-    path('login/', include('login.urls', namespace='login')),
     path('', include('shop.urls', namespace='shop')),
-    #url(r'^login/$', auth_views.login, name='login'),
-    #url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    path('login/', views.login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name= 'logout'),
+    url('social-auth/', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
