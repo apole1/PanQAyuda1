6214596550ce761e1387a37a3f0e3d161cbed852
Oswaldo1002009 <A01206725@itesm.mx>
2019-03-30 12:43:43 -0600

    Combined Oswaldo and Paulo branches

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -2,14 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from core import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
-    path('', include('shop.urls', namespace='shop')),
+    path('shop/', include('shop.urls', namespace='shop')),
     path('account/', include('django.contrib.auth.urls')),
+    path("login/", views.login, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('social-auth/', include('social_django.urls', namespace="social")),
+    path("signin/", views.home, name="home"),
+    path('maps/', include('maps.urls', namespace="maps")),
 ]
 
 if settings.DEBUG:
