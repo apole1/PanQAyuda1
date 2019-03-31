f2c1ea2cc9668676fa927fb97195ef5858f5bfd3
Oswaldo1002009 <A01206725@itesm.mx>
2019-03-31 11:13:47 -0600

    Deleted previous django application

--- PanQAyuda/login/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
-
-
-# Create your views here.
-
-def home(request):
-    return render(request, 'login/base.html')
-
