db95cf722f527b0517ca2c51a23d3b48ee5aaf73
Oswaldo1002009 <A01206725@itesm.mx>
2019-04-01 18:45:49 -0600

    Added User Login into core app

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
@@ -12,11 +13,19 @@ urlpatterns = [
     path('orders/', include('orders.urls', namespace='orders')),
     path('shop/', include('shop.urls', namespace='shop')),
     path('account/', include('django.contrib.auth.urls')),
+    
+    #SOCIAL LOGIN
     path("login/", views.social_login, name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('social-auth/', include('social_django.urls', namespace="social")),
-    path("signin/", views.home, name="home"),
+    path("social/", views.home, name="home"),
     path('maps/', include('maps.urls', namespace="maps")),
+
+    #USER LOGIN
+    url(r'^signin/',views.index,name='index'),
+    url(r'^special/',views.special,name='special'),
+    url(r'^core/',include('core.urls')),
+    url(r'^user_logout/$', views.user_logout, name='logout'),
 ]
 
 if settings.DEBUG:
