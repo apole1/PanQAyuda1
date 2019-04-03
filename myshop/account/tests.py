539ee822fc9adacccf7b8c156c7c62dc9ce5e9d1
Paulo Solis <a01207593@itesm.mx>
2019-04-03 15:42:52 -0600

    Añadir tests login

--- myshop/account/tests.py
+++ myshop/account/tests.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'paulo',
+            'password': 'prueba123'}
+        User.objects.create_user(**self.credentials)
+
+        self.credentials2 = {
+            'username': 'paulo',
+            'password': 'prueba1236'}
+
+    def test_login(self):
+        # send login data
+        response = self.client.post('/account/login/', self.credentials2, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
+
+    def test_login2(self):
+        # send login data
+        response = self.client.post('/account/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
+
