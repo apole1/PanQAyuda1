7e9059231ea9ac9ec676fa7fe65e54c1526b01c1
Oswaldo1002009 <A01206725@itesm.mx>
2019-04-20 16:00:23 -0500

    Add pagination and search bar to shop

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from django.core.paginator import Paginator
 
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products_list = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+        products_list = products_list.filter(category=category)
+    query = request.GET.get("q")
+    if query:
+        products_list = products_list.filter(name__icontains=query)
+    paginator = Paginator(products_list, 16) # Show 16 products per page
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
     return render(request,
                   'shop/product/list.html',
                   {'category': category,
                    'categories': categories,
                    'products': products})
-
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,
@@ -27,4 +33,3 @@ def product_detail(request, id, slug):
                   'shop/product/detail.html',
                   {'product': product,
                    'cart_product_form': cart_product_form})
-
