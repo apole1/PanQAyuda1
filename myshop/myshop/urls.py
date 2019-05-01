73ebad71bd926f5e1893f004f6845a50703d4372
Paulo Solis <a01207593@itesm.mx>
2019-05-01 13:49:16 -0500

    añadir api

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from api.resources import OrderResource
 from core import views
+
+order_resource = OrderResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,6 +20,7 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('social-auth/', include('social_django.urls', namespace="social")),
     path("signin/", views.home, name="home"),
+    path('api',include(order_resource.urls)),
     path('maps/', include('maps.urls', namespace="maps")),
 ]
 
