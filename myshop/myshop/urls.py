00a6689d3f312e9c04d2f79987a7ad03a96414e9
Oswaldo1002009 <A01206725@itesm.mx>
2019-05-05 11:00:53 -0500

    Restore traditional login lost in merge with develop

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,9 +22,14 @@ urlpatterns = [
     path("login/", views.social_login, name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('social-auth/', include('social_django.urls', namespace="social")),
-    path("signin/", views.home, name="home"),
+    path("social/", views.home, name="home"),
     path('api',include(order_resource.urls)),
     path('maps/', include('maps.urls', namespace="maps")),
+
+    url(r'^signin/',views.index,name='index'),
+    url(r'^special/',views.special,name='special'),
+    url(r'^core/',include('core.urls')),
+    url(r'^user_logout/$', views.user_logout, name='logout'),
 ]
 
 if settings.DEBUG:
