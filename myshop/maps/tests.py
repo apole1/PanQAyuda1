32f507226f43cfdc3cc727c403416ec2d9f0c52d
Oswaldo1002009 <A01206725@itesm.mx>
2019-05-09 17:24:44 -0500

    Add tests for sessions, social sessions, shop and maps

--- myshop/maps/tests.py
+++ myshop/maps/tests.py
@@ -1,10 +1,26 @@
 from django.test import TestCase
-import mapbox
-from mapbox.errors import TokenError
 from mapbox.services import base
 
-def test_service_session_env():
-    """Get a session using the env's token"""
-    session = base.Session(
-        env={'MapboxAccessToken': 'pk.test_env'})
-    assert session.params.get('access_token') == 'pk.test_env'
+class MapboxTest(TestCase):
+    def test_default_host(self):
+        service = base.Service()
+        self.assertEqual(service.host, 'api.mapbox.com')
+
+    def test_class_attrs(self):
+        """Get expected class attr values"""
+        serv = base.Service()
+        self.assertEqual(serv.api_name, 'hors service')
+        self.assertEqual(serv.api_version, 'v0')
+        self.assertEqual(serv.baseuri, 'https://api.mapbox.com/hors service/v0')
+
+    def test_service_session(self):
+        """Get a session using a token"""
+        session = base.Session('pk.test')
+        self.assertEqual(session.params.get('access_token'), 'pk.test')
+
+    def test_service_session_env(self):
+        """Get a session using the env's token"""
+        session = base.Session(
+            env={'MapboxAccessToken': 'pk.test_env'})
+        self.assertEqual(session.params.get('access_token'), 'pk.test_env')
+
--- myshop/shop/tests.py
+++ myshop/shop/tests.py
@@ -20,3 +20,30 @@ class OrderTest(TestCase):
         self.assertEqual(p.count, 60)
         self.assertEqual(p.num_pages, 5)
         self.assertEqual(list(p.page_range), [1, 2, 3, 4, 5])
+
+    #Not enough products to have more than one page
+    def test_false_next(self):
+        c = Category.objects.get(name="Test Categoria")
+        for i in range(5):
+            Product.objects.create(category=c, name=str(i), slug=slugify(str(i)), price=i,
+                                   available=True, created=models.DateTimeField(auto_now_add=True),
+                                   updated=models.DateTimeField(auto_now=True))
+        paginator = Paginator(Product.objects.all(), 12)
+        p = paginator.page(1)
+        self.assertFalse(p.has_next())
+        self.assertFalse(p.has_previous())
+
+    def test_true_next(self):
+        c = Category.objects.get(name="Test Categoria")
+        for i in range(58):
+            Product.objects.create(category=c, name=str(i), slug=slugify(str(i)), price=i,
+                                   available=True, created=models.DateTimeField(auto_now_add=True),
+                                   updated=models.DateTimeField(auto_now=True))
+        paginator = Paginator(Product.objects.all(), 12)
+        p = paginator.page(1)
+        self.assertEqual(12, p.end_index())
+        self.assertTrue(p.has_other_pages())
+        self.assertTrue(p.has_next())
+        p = paginator.page(5)
+        self.assertFalse(p.has_next())
+        self.assertEqual(58, p.end_index())
